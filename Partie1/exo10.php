6e1d09cfe5350d9b096abc7962b77bbb6dc12a46
Thigre-Corp <thigre.sspi@gmail.com>
2025-06-08 14:01:20 +0100

    Exo10 -OK

--- Partie1/exo10.php
+++ Partie1/exo10.php
@@ -1,9 +1,37 @@
-<h1>Exercice 1</h1>
+<h1>Exercice 10</h1>
 
 <p>
-
+    A partir d’un montant à payer et d’une somme versée pour régler un achat, écrire l’algorithme qui
+    affiche à un utilisateur un rendu de monnaie en nombre de billets de 10 € et 5 €, de pièces de 2 € et
+    1 €.
 </p>
 
 <h2>Résultat:</h2>
 
 <?php
+
+//déclaration de variables
+$prix = 152;
+$verse = 200;
+$reste = $verse - $prix;
+$monnaie = [10, 5, 2, 1];
+$nbMonnaie[] = 0 ;
+var_dump($nbMonnaie[0]);
+//affichage début
+echo "Montant à payer : $prix €<br>
+    Montant versé : $verse $<br>
+    Reste à payer : $reste € <br>
+    ***************************************************<br>
+    Rendue de monnaie :<br>";
+
+//calcul monnaie + affichage final
+for($i =0; $i <count($monnaie); $i++){
+    $nbMonnaie[$i] = intval($reste / $monnaie[$i]);
+    $reste -= $nbMonnaie[$i]*$monnaie[$i];
+    echo "$nbMonnaie[$i] ".
+        ($monnaie[$i]>5 ? "billet" : "pièce").
+        ($nbMonnaie[$i]>1 ? "s" : "").
+        " de $monnaie[$i] €".
+        ($i<(count($monnaie)-1) ? " - " : "<br>");
+}
+
