db92342e5e9328b1d60b28e3e984e135668ea4df
Thigre-Corp <thigre.sspi@gmail.com>
2025-06-08 09:57:33 +0100

    Exo1 -OK

--- Partie1/exo1.php
+++ Partie1/exo1.php
@@ -1,9 +1,15 @@
 <h1>Exercice 1</h1>
 
 <p>
-
+    Soit la phrase « Notre formation DL commence aujourd'hui ».
+    Ecrire un algorithme permettant de compter le nombre de caractères contenus dans cette phrase
+    (espaces inclus).
 </p>
 
 <h2>Résultat:</h2>
 
 <?php
+
+$laPhrase = "Notre formation DL commence aujourd'hui";
+
+echo "La phrase ".$laPhrase." contient ".strlen($laPhrase)." caractères.";
