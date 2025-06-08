180174dee308f8aefd409daebb572f2bd8c61a47
Thigre-Corp <thigre.sspi@gmail.com>
2025-06-08 10:33:26 +0100

    Exo3 -OK

--- Partie1/exo3.php
+++ Partie1/exo3.php
@@ -1,9 +1,16 @@
-<h1>Exercice 1</h1>
+<h1>Exercice 3</h1>
 
 <p>
-
+    A partir de la phrase de l’exercice 1, écrire l’instruction permettant de remplacer le mot
+    « aujourd’hui » par le mot « demain ». Afficher l’ancienne et la nouvelle phrase.
 </p>
 
 <h2>Résultat:</h2>
 
 <?php
+
+$laPhrase = "Notre formation DL commence aujourd'hui";
+
+echo " Avant instruction, la phrase vaut '".$laPhrase."<br>";
+$laPhrase  = str_replace("aujourd'hui", "demain", $laPhrase);
+echo " Après instruction, la phrase vaut '".$laPhrase."'.<br>";
