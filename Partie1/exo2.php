abbe73a0f3934ae7cf61f4c33165af20e291ebd3
Thigre-Corp <thigre.sspi@gmail.com>
2025-06-08 10:00:23 +0100

    Exo2 -OK

--- Partie1/exo2.php
+++ Partie1/exo2.php
@@ -1,9 +1,14 @@
-<h1>Exercice 1</h1>
+<h1>Exercice 2</h1>
 
 <p>
-
+    A partir de la phrase de l’exercice 1, écrire l’instruction permettant de compter le nombre de mots
+    contenus dans celle-ci.
 </p>
 
 <h2>Résultat:</h2>
 
 <?php
+
+$laPhrase = "Notre formation DL commence aujourd'hui";
+
+echo "La phrase ".$laPhrase." contient ".str_word_count($laPhrase)." mots.";
