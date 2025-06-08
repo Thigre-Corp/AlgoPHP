52391a2b2b5a96be614aca0dd444e80ba15b5042
Thigre-Corp <thigre.sspi@gmail.com>
2025-06-08 11:44:43 +0100

    Exo5 -OK

--- Partie1/exo5.php
+++ Partie1/exo5.php
@@ -1,9 +1,17 @@
-<h1>Exercice 1</h1>
+<h1>Exercice 5</h1>
 
 <p>
-
+    Ecrire un algorithme qui déclare une valeur en francs et qui la convertit en euros.
+    Attention, la valeur générée devra être arrondie à 2 décimales.
 </p>
 
 <h2>Résultat:</h2>
 
 <?php
+
+//déclarations de variables
+$prixF = 100;
+//déclaration de constante
+const TAUX_F_E = 6.55957;
+//affichage.
+echo "Le montant en francs est de : ".$prixF."<br>".$prixF." francs = ".(round($prixF / TAUX_F_E,2))." €<br>";
