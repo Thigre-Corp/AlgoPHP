f3b870f8d5da503bcc1a77b499b6df782c7c4214
Thigre-Corp <thigre.sspi@gmail.com>
2025-06-08 16:12:19 +0100

    Exo13 -OK

--- Partie1/exo13.php
+++ Partie1/exo13.php
@@ -1,9 +1,20 @@
-<h1>Exercice 1</h1>
+<h1>Exercice 13</h1>
 
 <p>
-
+    Calculer la moyenne générale d'un élève dont les notes sont renseignées dans un tableau (pas de
+    coefficient). Elle devra être affichée avec 2 décimales.
 </p>
 
 <h2>Résultat:</h2>
 
 <?php
+
+//déclaration de varaibles
+$notes = [10, 12, 8, 19, 3, 16, 11, 13, 9];
+
+//affichage
+echo "Les notes obtenues par l’élève sont :";
+foreach(range(0, count($notes)-1) as $i) {
+    echo " $notes[$i]";
+}
+echo "<br>Sa moyenne générale est de : ".round(array_sum($notes)/count($notes),2)."<br>";
